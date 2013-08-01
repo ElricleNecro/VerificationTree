31a4f8884f6e927c03a6e72416a310abf58487b7
ElricleNecro <guiplum@gmail.com>
2013-08-01 12:30:17 +0200

    Some object to get easy to add column to an existing dataset.

---
+++ include/HDF5.h
@@ -0,0 +1,16 @@
+#ifndef VERIF_HDF5
+#define VERIF_HDF5
+
+#include <hdf5.h>
+
+typedef struct _ExtensibleDataSet {
+	hsize_t size[2], offset[2];
+	hid_t dataset, file, grp;
+} *ExtensibleDataSet;
+
+//ExtensibleDataSet CreateExtensibleDS(hid_t id, const char *name, const char *sub_grp, const int lig, const int col);
+ExtensibleDataSet CreateExtensibleDS(hid_t id, const char *sub_grp, hsize_t dims[2]);
+void ExtensibleDataSet_Extend(ExtensibleDataSet id, const double *data, hsize_t dims[2]);
+void ExtensibleDataSet_Close(ExtensibleDataSet id);
+
+#endif /* end of include guard */
