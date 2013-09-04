ec394535f205014607e74867d6ffff8414e97eb0
ElricleNecro <guiplum@gmail.com>
2013-09-04 16:13:52 +0200

    Macroscopic temperature was calculated in the wrong way.

--- src/Verif_tools.c
+++ src/Verif_tools.c
@@ -696,10 +696,13 @@ double*  CalcTemperature(const TNoeud root, const double *densite, const int nb_
 
 	*Tmoy /= d_all;//root->N;
 
+	*Tmoy = 0.0;
 	for (int i = 0; i < nb_bin; i++) {
 		//Deltatemp[i] = temperature[i];
+		*Tmoy += temperature[i];
 		temperature[i] /= d_all;
 	}
+	*Tmoy /= d_all;//root->N;
 
 //	for(int i = 0; i < root->N; i++)
 //	{
